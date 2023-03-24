b3fb11dadee713ee37a065fbd5d1416567bca148
seyonthomas <seyonthomas01@gmail.com>
2023-03-24 15:53:49 -0700

    About to make a pull from develop to start building the edit warehouse page

--- src/pages/Warehouses/Warehouses.jsx
+++ src/pages/Warehouses/Warehouses.jsx
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-import './Warehouses.scss';
-import Footer from '../../components/Footer/Footer';
-
-function Warehouses() {
-    return (
-        <div>
-            <h1>Warehouse Page</h1>
-            <Footer />
-=======
 // import DeleteModal from "../../components/DeleteModal/DeleteModal";
 import "./Warehouses.scss";
 import SearchHeader from "../../components/SearchHeader/SearchHeader";
@@ -19,7 +9,6 @@ function Warehouses() {
         <div className="container">
             <SearchHeader />
             <WarehouseBody />
->>>>>>> develop
         </div>
     );
 }
