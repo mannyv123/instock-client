f3852e32bff2bead4c6ca61105f6d25fd2fee5e1
Manjot Virdi <virdi_manjot@hotmail.com>
2023-03-23 21:25:33 -0700

    Update warehouse components and modal

--- src/components/DeleteModal/DeleteModal.jsx
+++ src/components/DeleteModal/DeleteModal.jsx
@@ -5,20 +5,17 @@ import axios from "axios";
 
 const API_URL = "http://localhost:5001/api";
 
-function DeleteModal({ setIsOpen, location, type, typePlural }) {
-    //test variables
-    // const location = "Washington";
-    // const type = "warehouse";
-    // const typePlural = "warehouses";
-    let id = "90ac3319-70d1-4a51-b91d-ba6c2464408c";
+function DeleteModal({ isOpen, setIsOpen, warehouse, warehouseId }) {
+    const type = "warehouse";
+    const typePlural = "warehouses";
 
     const handleWarehouseDelete = () => {
         console.log("DELETE");
         axios
-            .delete(`${API_URL}/warehouses/${id}`)
+            .delete(`${API_URL}/warehouses/${warehouseId}`)
             .then(() => {
                 //reload the list of warehouses
-                console.log(`Warehouse with ID ${id} successfully deleted`);
+                console.log(`Warehouse with ID ${warehouseId} successfully deleted`);
                 setIsOpen(false);
             })
             .catch((error) => {
@@ -26,45 +23,54 @@ function DeleteModal({ setIsOpen, location, type, typePlural }) {
             });
     };
 
-    return (
-        <div className="modal">
-            <div className="modal__bg" onClick={() => setIsOpen(false)}></div>
-            <div className="modal__body">
-                <img
-                    className="modal__close"
-                    src={closeIcon}
-                    alt="close modal"
-                    onClick={() => setIsOpen(false)}
-                />
-                <div className="modal__content">
-                    <div className="modal__header">
-                        <h1 className="modal__title">
-                            Delete {location} {type}?
-                        </h1>
-                    </div>
+    console.log(warehouse);
 
-                    <p className="modal__text">
-                        Please confirm that you'd like to delete {location} from the list of
-                        {` ${typePlural}`}. You won't be able to undo this action.
-                    </p>
-                    <div className="modal__actions">
-                        <button
+    return (
+        <>
+            {!isOpen ? (
+                ""
+            ) : (
+                <div className="modal">
+                    <div className="modal__bg" onClick={() => setIsOpen(false)}></div>
+                    <div className="modal__body">
+                        <img
+                            className="modal__close"
+                            src={closeIcon}
+                            alt="close modal"
                             onClick={() => setIsOpen(false)}
-                            className="modal__button modal__button--cancel"
-                        >
-                            Cancel
-                        </button>
-                        <button
-                            type="button"
-                            onClick={handleWarehouseDelete}
-                            className="modal__button modal__button--delete"
-                        >
-                            Delete
-                        </button>
+                        />
+                        <div className="modal__content">
+                            <div className="modal__header">
+                                <h1 className="modal__title">
+                                    Delete {warehouse.warehouse_name} {type}?
+                                </h1>
+                            </div>
+
+                            <p className="modal__text">
+                                Please confirm that you'd like to delete {warehouse.warehouse_name} from the
+                                list of
+                                {` ${typePlural}`}. You won't be able to undo this action.
+                            </p>
+                            <div className="modal__actions">
+                                <button
+                                    onClick={() => setIsOpen(false)}
+                                    className="modal__button modal__button--cancel"
+                                >
+                                    Cancel
+                                </button>
+                                <button
+                                    type="button"
+                                    onClick={handleWarehouseDelete}
+                                    className="modal__button modal__button--delete"
+                                >
+                                    Delete
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
+            )}
+        </>
     );
 }
 
--- src/components/WarehouseBody/WarehouseBody.jsx
+++ src/components/WarehouseBody/WarehouseBody.jsx
@@ -1,14 +1,16 @@
-import './WarehouseBody.scss';
-import deleteIcon from '../../assets/icons/delete_outline-24px.svg';
-import editIcon from '../../assets/icons/edit-24px.svg';
-import { Link } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-export const apiUrl = 'http://localhost:5005/api';
+import "./WarehouseBody.scss";
+import deleteIcon from "../../assets/icons/delete_outline-24px.svg";
+import editIcon from "../../assets/icons/edit-24px.svg";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import DeleteModal from "../../components/DeleteModal/DeleteModal";
+export const apiUrl = "http://localhost:5001/api";
 
 function WarehouseBody() {
     const [warehouses, setWarehouses] = useState([]);
-
+    const [isOpen, setIsOpen] = useState(false);
     useEffect(() => {
         axios
             .get(`${apiUrl}/warehouses`)
@@ -20,52 +22,67 @@ function WarehouseBody() {
             });
     }, []);
 
+    console.log(warehouses);
+
     return (
-        <ul className='warehouse-table'>
-            <li className='warehouse-table__header'>
-                <div className='warehouse-table__col warehouse-table__col--1'>
+        <ul className="warehouse-table">
+            <li className="warehouse-table__header">
+                <div className="warehouse-table__col warehouse-table__col--1">
                     Warehouse
-                    <button className='sort'></button>
+                    <button className="sort"></button>
                 </div>
-                <div className='warehouse-table__col warehouse-table__col--2'>
+                <div className="warehouse-table__col warehouse-table__col--2">
                     Address
-                    <button className='sort'></button>
+                    <button className="sort"></button>
                 </div>
-                <div className='warehouse-table__col warehouse-table__col--3'>
+                <div className="warehouse-table__col warehouse-table__col--3">
                     Contact Name
-                    <button className='sort'></button>
+                    <button className="sort"></button>
                 </div>
-                <div className='warehouse-table__col warehouse-table__col--4'>
+                <div className="warehouse-table__col warehouse-table__col--4">
                     Contact Information
-                    <button className='sort'></button>
+                    <button className="sort"></button>
                 </div>
-                <div className='warehouse-table__col warehouse-table__col--5'>Actions</div>
+                <div className="warehouse-table__col warehouse-table__col--5">Actions</div>
             </li>
 
             {warehouses.map((warehouse) => (
-                <li className='warehouse-table__row' key={warehouse.id}>
-                    <div className='warehouse-table__col warehouse-table__col--1' data-label='Warehouse'>
-                        <Link to={`/warehouses/${warehouse.id}`} className='warehouse-table__col--title'>
+                <li className="warehouse-table__row" key={warehouse.id}>
+                    <div className="warehouse-table__col warehouse-table__col--1" data-label="Warehouse">
+                        <Link to={`/warehouses/${warehouse.id}`} className="warehouse-table__col--title">
                             {warehouse.warehouse_name}
                         </Link>
                     </div>
-                    <div className='warehouse-table__col warehouse-table__col--2' data-label='Address'>
+                    <div className="warehouse-table__col warehouse-table__col--2" data-label="Address">
                         {warehouse.address}, {warehouse.city}, {warehouse.country}
                     </div>
-                    <div className='warehouse-table__col warehouse-table__col--3' data-label='Contact Name'>
+                    <div className="warehouse-table__col warehouse-table__col--3" data-label="Contact Name">
                         {warehouse.contact_name}
                     </div>
-                    <div className='warehouse-table__col warehouse-table__col--4' data-label='Contact Information'>
+                    <div
+                        className="warehouse-table__col warehouse-table__col--4"
+                        data-label="Contact Information"
+                    >
                         {warehouse.contact_phone}
                         <br />
                         {warehouse.contact_email}
                     </div>
-                    <div className='warehouse-table__col warehouse-table__col--5' data-label='Actions'>
-                        <button className='warehouse-table__col--btn'>
-                            <img src={deleteIcon} alt='delete' />
+                    <div className="warehouse-table__col warehouse-table__col--5" data-label="Actions">
+                        <button onClick={() => setIsOpen(true)} className="warehouse-table__col--btn">
+                            <img src={deleteIcon} alt="delete" />
                         </button>
-                        <button className='warehouse-table__col--btn'>
-                            <img src={editIcon} alt='edit' />
+                        {/* {isOpen && ( */}
+                        <DeleteModal
+                            isOpen={isOpen}
+                            setIsOpen={setIsOpen}
+                            warehouse={warehouse}
+                            warehouseId={warehouse.id}
+                        />
+                        {/* )} */}
+
+                        {console.log(`warehouse name ${warehouse.warehouse_name}`)}
+                        <button className="warehouse-table__col--btn">
+                            <img src={editIcon} alt="edit" />
                         </button>
                     </div>
                 </li>
