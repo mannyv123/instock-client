99345a2728d92155abe2a9b4016b4c6637a82d9d
Manjot Virdi <virdi_manjot@hotmail.com>
2023-03-24 11:52:21 -0700

    update new inventory form; update styling of form; add default pointer cursor for buttons

--- src/pages/AddInventoryPage/AddInventoryPage.jsx
+++ src/pages/AddInventoryPage/AddInventoryPage.jsx
@@ -1,7 +1,25 @@
 import "./AddInventoryPage.scss";
 import arrowBack from "../../assets/icons/arrow_back-24px.svg";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const apiUrl = "http://localhost:5001/api";
 
 function AddInventoryPage() {
+    const [warehouses, setWarehouses] = useState([]);
+
+    //Get current warehouses for dropdown selection
+    useEffect(() => {
+        axios
+            .get(`${apiUrl}/warehouses`)
+            .then((response) => {
+                setWarehouses(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+
     return (
         <section className="container">
             <section className="new-inventory">
@@ -34,7 +52,7 @@ function AddInventoryPage() {
                         <label className="new-inventory__label" htmlFor="category">
                             Category
                         </label>
-                        <select className="new-inventory__category" name="category" id="category">
+                        <select className="new-inventory__dropdown" name="category" id="category">
                             <option value="">Please select</option>
                             <option value="accessories">Accessories</option>
                             <option value="electronics">Electronics</option>
@@ -42,6 +60,65 @@ function AddInventoryPage() {
                             <option value="health">Health</option>
                             <option value="apparel">Apparel</option>
                         </select>
+                    </div>
+                    <div className="new-inventory__item-avail">
+                        <h2 className="new-inventory__sub-header">Item Availability</h2>
+                        <div className="new-inventory__status-container">
+                            <legend className="new-inventory__label new-inventory__label--status">
+                                Status
+                            </legend>
+                            <div className="new-inventory__radio">
+                                <input
+                                    className="new-inventory__status-input"
+                                    type="radio"
+                                    name="status"
+                                    id="inStock"
+                                    value="inStock"
+                                />
+                                <label htmlFor="inStock">In Stock</label>
+                            </div>
+                            <div className="new-inventory__radio">
+                                <input
+                                    className="new-inventory__status-input"
+                                    type="radio"
+                                    name="status"
+                                    id="outOfStock"
+                                    value="outOfStock"
+                                />
+                                <label htmlFor="outOfStock">Out of Stock</label>
+                            </div>
+                        </div>
+                        <label htmlFor="quantity" className="new-inventory__label">
+                            Quantity
+                        </label>
+                        <input
+                            className="new-inventory__input"
+                            type="number"
+                            name="quantity"
+                            id="quantity"
+                            placeholder="0"
+                        />
+                        <label className="new-inventory__label" htmlFor="warehouse">
+                            Warehouse
+                        </label>
+                        <select className="new-inventory__dropdown" name="category" id="category">
+                            <option value="">Please select</option>
+                            {warehouses.map((warehouse) => {
+                                return (
+                                    <option key={warehouse.id} value={warehouse.warehouse_name}>
+                                        {warehouse.warehouse_name}
+                                    </option>
+                                );
+                            })}
+                        </select>
+                    </div>
+                    <div className="new-inventory__form-actions">
+                        <button className="new-inventory__btn new-inventory__btn--cancel" type="button">
+                            Cancel
+                        </button>
+                        <button className="new-inventory__btn new-inventory__btn--add" type="submit">
+                            + Add Item
+                        </button>
                     </div>
                 </form>
             </section>
