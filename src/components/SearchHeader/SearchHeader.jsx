1c31080d2ca835a950bad231f8563465cf4b3b48
gjpineda <gjpineda@live.com>
2023-03-26 12:16:00 -0700

    Fixed Add New Warehouse/Add New Item buttons

--- src/components/SearchHeader/SearchHeader.jsx
+++ src/components/SearchHeader/SearchHeader.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./SearchHeader.scss";
 import { Link } from "react-router-dom";
 
-function SearchHeader({ title }) {
+function SearchHeader({ title, buttonLink, buttonTitle }) {
   return (
     <div className="search-header">
       <h1 className="search-header__title">{title}</h1>
@@ -14,8 +14,8 @@ function SearchHeader({ title }) {
         ></input>
         {/* I changed the button here -GJ */}
         {/* <button className="search-header__button">+ Add New Warehouse</button> */}
-        <Link className="search-header__button" to="/warehouses/add">
-          + Add New Warehouse
+        <Link className="search-header__button" to={buttonLink}>
+          {buttonTitle}
         </Link>
       </form>
     </div>
--- src/pages/Warehouses/Warehouses.jsx
+++ src/pages/Warehouses/Warehouses.jsx
@@ -1,16 +1,20 @@
 // import DeleteModal from "../../components/DeleteModal/DeleteModal";
-import './Warehouses.scss';
-import SearchHeader from '../../components/SearchHeader/SearchHeader';
-import WarehouseBody from '../../components/WarehouseBody/WarehouseBody';
+import "./Warehouses.scss";
+import SearchHeader from "../../components/SearchHeader/SearchHeader";
+import WarehouseBody from "../../components/WarehouseBody/WarehouseBody";
 
 function Warehouses() {
-    return (
-        //    Felipe's code
-        <div className='container'>
-            <SearchHeader title='Warehouses' />
-            <WarehouseBody />
-        </div>
-    );
+  return (
+    //    Felipe's code
+    <div className="container">
+      <SearchHeader
+        title="Inventory"
+        buttonLink="/warehouses/add"
+        buttonTitle="+ Add New Warehouse"
+      />
+      <WarehouseBody />
+    </div>
+  );
 }
 
 export default Warehouses;
