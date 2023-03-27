b0b8779c11ce3d3bb88f9aa44aec6aec6b8901ff
Felipe Gonzalez <fgonzalezcare@gmail.com>
2023-03-26 19:15:57 -0700

    fix some issues with ticket 21

--- src/components/InventoryItem/InventoryItem.jsx
+++ src/components/InventoryItem/InventoryItem.jsx
@@ -9,7 +9,7 @@ import { Link } from 'react-router-dom';
 const apiUrl = 'http://localhost:5005/api';
 
 function InventoryItem() {
-     const [selectedInventory, setSelectedInventory] = useState([]);
+     const [selectedInventory, setSelectedInventory] = useState({});
      const [warehouse] = useState({});
      const { id } = useParams();
 
@@ -37,37 +37,39 @@ function InventoryItem() {
                                    <Link to={`/inventory/`} className='warehouse-header__comeback'>
                                         <img src={iconReturn} alt='back to inventory' />
                                    </Link>
-                                   <h1 className='warehouse-header__text'>{selectedInventory.length > 0 && selectedInventory[0].item_name}</h1>
+                                   <h1 className='warehouse-header__text'>
+                                        {Object.keys(selectedInventory).length > 0 && selectedInventory.item_name}
+                                   </h1>
                               </div>
                               <Link to={`/warehouse/${warehouse.id}/edit`} className='warehouse-header__button' data-label='Edit'></Link>
                          </div>
-                         {selectedInventory[0] && (
-                              <section className='details-grid' key={InventoryItem}>
+                         {selectedInventory && (
+                              <section className='details-grid'>
                                    <div className='grid-item description'>
                                         <h4 className='grid-item__title'>item description:</h4>
-                                        <p className='grid-item__text'>{selectedInventory[0].description}</p>
+                                        <p className='grid-item__text'>{selectedInventory.description}</p>
                                    </div>
                                    <div className='grid-item category'>
                                         <h4 className='grid-item__title'>Category:</h4>
-                                        <p className='grid-item__text'>{selectedInventory[0].category}</p>
+                                        <p className='grid-item__text'>{selectedInventory.category}</p>
                                    </div>
                                    <div className='grid-item status'>
                                         <h4 className='grid-item__title'>Status:</h4>
                                         <p className='grid-item__text'>
-                                             {selectedInventory[0].status === 'In Stock' ? (
-                                                  <span className='grid-item__text--success'>{selectedInventory[0].status}</span>
+                                             {selectedInventory.status === 'In Stock' ? (
+                                                  <span className='grid-item__text--success'>{selectedInventory.status}</span>
                                              ) : (
-                                                  <span className='grid-item__text--failed'>{selectedInventory[0].status}</span>
+                                                  <span className='grid-item__text--failed'>{selectedInventory.status}</span>
                                              )}
                                         </p>
                                    </div>
                                    <div className='grid-item quantity'>
                                         <h4 className='grid-item__title'>Quantity:</h4>
-                                        <p className='grid-item__text'>{selectedInventory[0].quantity}</p>
+                                        <p className='grid-item__text'>{selectedInventory.quantity}</p>
                                    </div>
                                    <div className='grid-item warehouse'>
                                         <h4 className='grid-item__title'>Warehouse:</h4>
-                                        <p className='grid-item__text'>{selectedInventory[0].warehouse_id}</p>
+                                        <p className='grid-item__text'>{selectedInventory.warehouse_name}</p>
                                    </div>
                               </section>
                          )}
