896351771a253b689351fb010e224736ad552bea
Manjot Virdi <virdi_manjot@hotmail.com>
2023-03-27 15:18:17 -0700

    update error msg component to include msg for quantity; update inventory item component to fix navigation to edit inventory page; update add inventory page styling

--- src/components/ErrorMsg/ErrorMsg.jsx
+++ src/components/ErrorMsg/ErrorMsg.jsx
@@ -2,11 +2,14 @@ import "./ErrorMsg.scss";
 import errorIcon from "../../assets/icons/error-24px.svg";
 
 //Error message rendered when field is invalid
-function ErrorMsg() {
+function ErrorMsg({ isInvalidQuantity }) {
     return (
         <div className="error-msg">
-            <img className="error-msg__icon" src={errorIcon} alt="error" />
-            <p>This field is required</p>
+            <div className="error-msg__main">
+                <img className="error-msg__icon" src={errorIcon} alt="error" />
+                <p>This field is required.</p>
+            </div>
+            {isInvalidQuantity && <p>This field cannot be less than zero or zero if in stock.</p>}
         </div>
     );
 }
