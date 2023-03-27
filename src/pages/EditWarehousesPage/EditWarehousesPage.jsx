bebf608b8ffed15188a47113e920e3dff016c5b9
seyonthomas <seyonthomas01@gmail.com>
2023-03-27 15:02:52 -0700

    Updated header and footer

--- src/pages/EditWarehousesPage/EditWarehousesPage.jsx
+++ src/pages/EditWarehousesPage/EditWarehousesPage.jsx
@@ -1,104 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-import "./EditWarehousesPage.scss";
-import { useParams } from "react-router-dom";
-import { useCallback, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
-
-const initialValues = {
-    // Warehouse Details
-    warehouse_name: "",
-    address: "",
-    city: "",
-    country: "",
-    // Contact Details
-    contact_name: "",
-    contact_position: "",
-    contact_phone: "",
-    contact_email: "",
-  };
-
-function EditWarehousesPage() {
-    const api_url = "http://localhost:8000";
-
-    const { id } = useParams();
-    const navigate = useNavigate();
-    const [warehouse, setWarehouse] = useState(initialValues);
-    const [isError, setIsError] = useState(false);
-  
-    function handleOnBack(event) {
-      // This prevents refreshing the page when you submit something.
-      event.preventDefault();
-      return navigate("/");
-    }
-  
-    function handleSubmit(event) {
-      event.preventDefault();
-  
-      const re_email = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}"); // matches w something@something.com
-      const re_phone = new RegExp("[0-9]{3}[0-9]{3}[0-9]{4}"); // matches w 1231231234
-  
-      if (
-        warehouse.warehouse_name === "" ||
-        warehouse.address === "" ||
-        warehouse.city === "" ||
-        warehouse.country === "" ||
-        warehouse.contact_name === "" ||
-        warehouse.contact_position === "" ||
-        warehouse.contact_phone === "" ||
-        !re_phone.test(warehouse.contact_phone) ||
-        warehouse.contact_email === "" ||
-        !re_email.test(warehouse.contact_email)
-      ) {
-        setIsError(true);
-        alert("error!");
-      } else {
-        setIsError(false);
-        alert("submitted!");
-        return navigate("/");
-      }
-    }
-  
-    // This handles the onChange for all inputs.
-    function handleWarehouseOnChange(event) {
-      const { name, value } = event.target;
-      setWarehouse({
-        ...warehouse,
-        [name]: value,
-      });
-    }
-  
-    const getWarehouseDetails = useCallback(
-        (id) => {
-          if (id) {
-            axios.get(`${api_url}/api/warehouses/${id}`).then((response) => {
-              setWarehouse(response.data);
-            });
-          }
-        },
-        [id]
-      );
-
-      useEffect(() => {
-        getWarehouseDetails(id);
-      }, [getWarehouseDetails]);
-
-
-    async function putUpdatedWarehouse() {
-      try {
-        const response = await axios.put(`${api_url}/api/warehouses/${id}`, warehouse);
-        console.log(response);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-
-
-  return (
-    <div>
-        <section className="container">
-=======
 import ErrorMsg from "../../components/ErrorMsg/ErrorMsg";
 // GJ's work
 import "./EditWarehousesPage.scss";
@@ -235,7 +134,6 @@ function WarehousesEdit() {
   return (
     <>
       <section className="container">
->>>>>>> develop
         <form className="content" onSubmit={handleSubmit}>
           <div className="content__sub-container">
             <a className="content__arrow-back" onClick={handleOnBack}>
@@ -247,13 +145,6 @@ function WarehousesEdit() {
             <div className="content__details-container">
               <div className="content__input-container">
                 <h2 className="content__title">Warehouse Details</h2>
-<<<<<<< HEAD
-                <h3 className="content__input-label">Warehouse Name</h3>
-                <input
-                  name="warehouse_name"
-                  onChange={handleWarehouseOnChange}
-                  className={`content__input ${isError ? "error-state" : ""}`}
-=======
                 <h3 className="content__input-label content__input-label--first">
                   Warehouse Name
                 </h3>
@@ -263,84 +154,52 @@ function WarehousesEdit() {
                   className={`content__input-txt ${
                     isErrorWarehouseName ? "error-state" : ""
                   }`}
->>>>>>> develop
                   type="text"
                   value={warehouse.warehouse_name}
                   placeholder="Warehouse Name"
                 />
-<<<<<<< HEAD
-=======
                 {isErrorWarehouseName && <ErrorMsg />}
->>>>>>> develop
                 <h3 className="content__input-label">Street Address</h3>
                 <input
                   name="address"
                   onChange={handleWarehouseOnChange}
-<<<<<<< HEAD
-                  className={`content__input ${isError ? "error-state" : ""}`}
-=======
                   className={`content__input-txt ${
                     isErrorAddress ? "error-state" : ""
                   }`}
->>>>>>> develop
                   type="text"
                   value={warehouse.address}
                   placeholder="Street Address"
                 />
-<<<<<<< HEAD
-=======
                 {isErrorAddress && <ErrorMsg />}
->>>>>>> develop
                 <h3 className="content__input-label">City</h3>
                 <input
                   name="city"
                   onChange={handleWarehouseOnChange}
-<<<<<<< HEAD
-                  className={`content__input ${isError ? "error-state" : ""}`}
-=======
                   className={`content__input-txt ${
                     isErrorCity ? "error-state" : ""
                   }`}
->>>>>>> develop
                   type="text"
                   value={warehouse.city}
                   placeholder="City"
                 />
-<<<<<<< HEAD
-=======
                 {isErrorCity && <ErrorMsg />}
->>>>>>> develop
                 <h3 className="content__input-label">Country</h3>
                 <input
                   name="country"
                   onChange={handleWarehouseOnChange}
-<<<<<<< HEAD
-                  className={`content__input ${isError ? "error-state" : ""}`}
-=======
                   className={`content__input-txt ${
                     isErrorCountry ? "error-state" : ""
                   }`}
->>>>>>> develop
                   type="text"
                   value={warehouse.country}
                   placeholder="Country"
                 />
-<<<<<<< HEAD
-=======
                 {isErrorCountry && <ErrorMsg />}
->>>>>>> develop
               </div>
             </div>
 
             <div className="content__details-container">
               <h2 className="content__title">Contact Details</h2>
-<<<<<<< HEAD
-              <h3 className="content__input-label">Contact Name</h3>
-              <input
-                name="contact_name"
-                onChange={handleWarehouseOnChange}
-                className={`content__input ${isError ? "error-state" : ""}`}
-=======
               <h3 className="content__input-label content__input-label--first">
                 Contact Name
               </h3>
@@ -350,72 +209,47 @@ function WarehousesEdit() {
                 className={`content__input-txt ${
                   isErrorContactName ? "error-state" : ""
                 }`}
->>>>>>> develop
                 type="text"
                 value={warehouse.contact_name}
                 placeholder="Contact Name"
               />
-<<<<<<< HEAD
-=======
               {isErrorContactName && <ErrorMsg />}
->>>>>>> develop
               <h3 className="content__input-label">Position</h3>
               <input
                 name="contact_position"
                 onChange={handleWarehouseOnChange}
-<<<<<<< HEAD
-                className={`content__input ${isError ? "error-state" : ""}`}
-=======
                 className={`content__input-txt ${
                   isErrorContactPosition ? "error-state" : ""
                 }`}
->>>>>>> develop
                 type="text"
                 value={warehouse.contact_position}
                 placeholder="Position"
               />
-<<<<<<< HEAD
-=======
               {isErrorContactPosition && <ErrorMsg />}
->>>>>>> develop
               <h3 className="content__input-label">Phone Number</h3>
               <input
                 name="contact_phone"
                 onChange={handleWarehouseOnChange}
-<<<<<<< HEAD
-                className={`content__input ${isError ? "error-state" : ""}`}
-=======
                 className={`content__input-txt ${
                   isErrorContactPhone ? "error-state" : ""
                 }`}
->>>>>>> develop
                 type="text"
                 value={warehouse.contact_phone}
                 placeholder="Phone Number"
               />
-<<<<<<< HEAD
-=======
               {isErrorContactPhone && <ErrorMsg />}
->>>>>>> develop
               <h3 className="content__input-label">Email</h3>
               <input
                 name="contact_email"
                 onChange={handleWarehouseOnChange}
-<<<<<<< HEAD
-                className={`content__input ${isError ? "error-state" : ""}`}
-=======
                 className={`content__input-txt ${
                   isErrorContactEmail ? "error-state" : ""
                 }`}
->>>>>>> develop
                 type="text"
                 value={warehouse.contact_email}
                 placeholder="Email"
               />
-<<<<<<< HEAD
-=======
               {isErrorContactEmail && <ErrorMsg />}
->>>>>>> develop
             </div>
           </div>
           <div className="content__buttons-container">
@@ -426,16 +260,8 @@ function WarehousesEdit() {
           </div>
         </form>
       </section>
-<<<<<<< HEAD
-    </div>
-  )
-}
-
-export default EditWarehousesPage
-=======
     </>
   );
 }
 
 export default WarehousesEdit;
->>>>>>> develop
