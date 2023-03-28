c22d4359678ad6e3a2dc08788934a9be36fc50c3
Manjot Virdi <virdi_manjot@hotmail.com>
2023-03-27 17:29:34 -0700

    update error msg component to take in more props; update add warehouse component to define format of phone numbers

--- src/components/ErrorMsg/ErrorMsg.jsx
+++ src/components/ErrorMsg/ErrorMsg.jsx
@@ -2,7 +2,7 @@ import "./ErrorMsg.scss";
 import errorIcon from "../../assets/icons/error-24px.svg";
 
 //Error message rendered when field is invalid
-function ErrorMsg({ isInvalidQuantity }) {
+function ErrorMsg({ isInvalidQuantity, isErrorContactPhone, isErrorContactEmail }) {
     return (
         <div className="error-msg">
             <div className="error-msg__main">
@@ -10,6 +10,8 @@ function ErrorMsg({ isInvalidQuantity }) {
                 <p>This field is required.</p>
             </div>
             {isInvalidQuantity && <p>This field cannot be less than zero or zero if in stock.</p>}
+            {isErrorContactPhone && <p>This field must be in the format ###-###-####.</p>}
+            {isErrorContactEmail && <p>This field must be in the format something@something.com.</p>}
         </div>
     );
 }
