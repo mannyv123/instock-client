9f976957c9d0bb31d93cdea827cffda511eadd95
Felipe Gonzalez <fgonzalezcare@gmail.com>
2023-03-27 20:34:44 -0700

    add Links path to edit buttons

--- src/components/DetailsBody/DetailsBody.jsx
+++ src/components/DetailsBody/DetailsBody.jsx
@@ -85,9 +85,9 @@ function DetailsBody() {
                                              }}>
                                              <img className='warehouse-details__col--img' src={deleteIcon} alt='delete' />
                                         </button>
-                                        <button className='warehouse-details__col--btn'>
+                                        <Link to={`/inventory/${inventory.id}/edit`} className='warehouse-details__col--btn'>
                                              <img className='warehouse-details__col--img' src={editIcon} alt='edit' />
-                                        </button>
+                                        </Link>
                                    </div>
                               </li>
                          ))}
