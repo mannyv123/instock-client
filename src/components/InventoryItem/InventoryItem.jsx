984f526860943473a0e88144716760881c08479e
Manjot Virdi <virdi_manjot@hotmail.com>
2023-03-27 21:06:27 -0700

    remove unused state variable

--- src/components/InventoryItem/InventoryItem.jsx
+++ src/components/InventoryItem/InventoryItem.jsx
@@ -9,7 +9,6 @@ import { apiUrl } from "../../App";
 
 function InventoryItem() {
     const [selectedInventory, setSelectedInventory] = useState({});
-    // const [warehouse] = useState({});
     const { id } = useParams();
 
     useEffect(() => {
