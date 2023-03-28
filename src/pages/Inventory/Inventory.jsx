a1146aacb4c7f949e781505d44ea08e608376893
Manjot Virdi <virdi_manjot@hotmail.com>
2023-03-27 20:58:44 -0700

    remove commented out code

--- src/pages/Inventory/Inventory.jsx
+++ src/pages/Inventory/Inventory.jsx
@@ -1,4 +1,3 @@
-// import InventoryItem from "../../components/InventoryItem/InventoryItem";
 import "./Inventory.scss";
 import InventoryList from "../../components/InventoryList/InventoryList";
 import { apiUrl } from "../../App";
@@ -7,34 +6,31 @@ import { useEffect, useState } from "react";
 
 // ------JIRA TICKET #J2VT1-20 -SEYON -------------------------------------
 function Inventory() {
-  const [inventoryItems, setInventoryItems] = useState([]);
+    const [inventoryItems, setInventoryItems] = useState([]);
 
-  useEffect(() => {
-    generateInventoryItems();
-  }, []);
-   // diving deeper -GJ
-  function generateInventoryItems(sort_by = "inventory_item", isAsc = true) {
-    const order_by = isAsc ? "asc" : "desc";
-    axios
-      .get(`${apiUrl}/inventories?sort_by=${sort_by}&order_by=${order_by}`)
-      .then((res) => {
-        setInventoryItems(res.data);
-      })
-      .catch((err) => {
-        console.log("err: ", err);
-      });
-  }
+    useEffect(() => {
+        generateInventoryItems();
+    }, []);
+    // diving deeper -GJ
+    function generateInventoryItems(sort_by = "inventory_item", isAsc = true) {
+        const order_by = isAsc ? "asc" : "desc";
+        axios
+            .get(`${apiUrl}/inventories?sort_by=${sort_by}&order_by=${order_by}`)
+            .then((res) => {
+                setInventoryItems(res.data);
+            })
+            .catch((err) => {
+                console.log("err: ", err);
+            });
+    }
 
-  // -----------------SEYON CODE END----------------------------------------
+    // -----------------SEYON CODE END----------------------------------------
 
-  return (
-    <div className="container">
-      <InventoryList
-        inventoryItems={inventoryItems}
-        generateInventoryItems={generateInventoryItems}
-      />
-    </div>
-  );
+    return (
+        <div className="container">
+            <InventoryList inventoryItems={inventoryItems} generateInventoryItems={generateInventoryItems} />
+        </div>
+    );
 }
 
 export default Inventory;
